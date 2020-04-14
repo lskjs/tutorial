597b9a7b2cc6ff88b87491a9583fa98e9bb97e04
Andrey Kondalov <i@yukio.ru>
2020-04-14 15:21:16 +0400

    Step 4.1: add @lskjs/build-locales

---
+++ .lskjs.js
@@ -0,0 +1,5 @@
+module.exports = {
+  i18: {
+    url: 'https://docs.google.com/spreadsheets/d/1m1PXq0FIhFefqIxd3WTPugqMTcKoHsPuU6SlmnxG1Pw/edit#gid=0',
+  },
+};
--- src/i18.js
+++ src/i18.js
@@ -1,24 +1,16 @@
 import I18 from '@lskjs/i18';
+import enResource from './translations/en.json';
+import ruResource from './translations/ru.json';
 
 const i18 = new I18({
   config: {
     locales: ['en', 'ru'],
     resources: {
       en: {
-        translation: {
-          helloWorld: 'Hello World',
-          button: {
-            changeLanguage: 'Change language',
-          },
-        },
+        translation: enResource,
       },
       ru: {
-        translation: {
-          helloWorld: 'Привет Мир',
-          button: {
-            changeLanguage: 'Сменить язык',
-          },
-        },
+        translation: ruResource,
       },
     },
   },
